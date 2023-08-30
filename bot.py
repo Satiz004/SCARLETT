cd8419ae2327379f57647d47addbc0e9d2c4c2e3
Satiz004 <138674478+Satiz004@users.noreply.github.com>
2023-08-30 19:41:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import logging.config
  # Get logging configurations 
 logging.config.fileConfig('logging.conf') 
 logging.getLogger().setLevel(logging.INFO) 
- logging.getLogger("pyrogram").setLevel(logging.ERROR) 
- logging.getLogger("imdbpy").setLevel(logging.ERROR) 
+logging.getLogger("pyrogram").setLevel(logging.ERROR) 
+logging.getLogger("imdbpy").setLevel(logging.ERROR) 
   
 from pyrogram import Client, __version__ 
 from pyrogram.raw.all import layer 
